a915fb253702ed66fe1b4615a0c4164ccf6413f6
disorientationinspace <disorientationinspace@gmail.com>
2020-06-25 12:26:45 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const { src, dest, watch, parallel, series } = require("gulp");
+const sass           = require("gulp-sass");
+const browserSync    = require("browser-sync");
+const autoprefixer   = require("gulp-autoprefixer");
+const cleancss       = require("gulp-clean-css");
+
+function styles() {
+  return src("app/sass/**/*.scss")
+        .pipe(sass())
+        .pipe(autoprefixer( { overrideBrowserlist: ["last 15 versions"] } ))
+        .pipe(cleancss( ( {format: "beautify"} ) ))
+        .pipe(dest("app/css"))
+        .pipe(browserSync.stream())
+}
+
+
+function browsersync() {
+  browserSync.init( {
+    server: {
+      baseDir: "app/"
+    },
+    notify: false,
+    online: true,
+  } )
+}
+
+function startwatch() {
+  watch(["app/sass/**/*.scss"], styles);
+  watch("app/**/*.html").on("change", browserSync.reload);
+}
+
+
+exports.default    = parallel(styles, browsersync, startwatch);
