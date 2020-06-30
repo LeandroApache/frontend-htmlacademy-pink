ae2d782559e55a59ad8bb790c57fb701b56807af
disorientationinspace <disorientationinspace@gmail.com>
2020-06-30 17:52:11 +0300

    Fixed reviews styles + slider

--- app/js/reviews-slider.js
+++ app/js/reviews-slider.js
@@ -1,43 +1,43 @@
-var radioButtons = document.querySelectorAll(".slider__input-radio[name='review']");
-var slides = document.querySelectorAll(".reviews .slider__element");
-var buttonNext = document.querySelector(".reviews__button-next");
-var buttonPrev = document.querySelector(".reviews__button-previous");
+var reviewsRadioButtons = document.querySelectorAll(".slider__input-radio[name='review']");
+var reviewsSlides = document.querySelectorAll(".reviews .slider__element");
+var reviewsButtonNext = document.querySelector(".reviews__button-next");
+var reviewsButtonPrev = document.querySelector(".reviews__button-previous");
 
-var activeSlide = 0;
+var reviewsActiveSlide = 0;
 
-buttonNext.addEventListener("click", function() {
-  activeSlide += 1;
-  if (activeSlide >= slides.length) {
-    slides[activeSlide - 1].classList.remove("slider__element--active", "fadeIn");
-    activeSlide = 0;
-    slides[activeSlide].classList.add("slider__element--active", "fadeIn");
+reviewsButtonNext.addEventListener("click", function() {
+  reviewsActiveSlide += 1;
+  if (reviewsActiveSlide >= reviewsSlides.length) {
+    reviewsSlides[reviewsActiveSlide - 1].classList.remove("slider__element--active", "fadeIn");
+    reviewsActiveSlide = 0;
+    reviewsSlides[reviewsActiveSlide].classList.add("slider__element--active", "fadeIn");
   } else {
-    slides[activeSlide - 1].classList.remove("slider__element--active", "fadeIn");
-    slides[activeSlide].classList.add("slider__element--active", "fadeIn");
+    reviewsSlides[reviewsActiveSlide - 1].classList.remove("slider__element--active", "fadeIn");
+    reviewsSlides[reviewsActiveSlide].classList.add("slider__element--active", "fadeIn");
   }
 })
 
-buttonPrev.addEventListener("click", function() {
-  activeSlide -= 1;
-  if (activeSlide < 0) {
-    slides[activeSlide + 1].classList.remove("slider__element--active", "fadeIn");
-    activeSlide = 2;
-    slides[activeSlide].classList.add("slider__element--active", "fadeIn");
+reviewsButtonPrev.addEventListener("click", function() {
+  reviewsActiveSlide -= 1;
+  if (reviewsActiveSlide < 0) {
+    reviewsSlides[reviewsActiveSlide + 1].classList.remove("slider__element--active", "fadeIn");
+    reviewsActiveSlide = 2;
+    reviewsSlides[reviewsActiveSlide].classList.add("slider__element--active", "fadeIn");
   } else {
-    slides[activeSlide + 1].classList.remove("slider__element--active", "fadeIn");
-    slides[activeSlide].classList.add("slider__element--active", "fadeIn");
+    reviewsSlides[reviewsActiveSlide + 1].classList.remove("slider__element--active", "fadeIn");
+    reviewsSlides[reviewsActiveSlide].classList.add("slider__element--active", "fadeIn");
   }
 })
 
 
-for (var i = 0; i < radioButtons.length; i++) {
-  radioButtons[i].addEventListener("click", function(evt) {
-    if ((this.value - 1) == activeSlide) {
+for (var i = 0; i < reviewsRadioButtons.length; i++) {
+  reviewsRadioButtons[i].addEventListener("click", function(evt) {
+    if ((this.value - 1) == reviewsActiveSlide) {
       //pass
     } else {
-      slides[activeSlide].classList.remove("slider__element--active", "fadeIn");
-      activeSlide = this.value - 1;
-      slides[activeSlide].classList.add("slider__element--active", "fadeIn");
+      reviewsSlides[reviewsActiveSlide].classList.remove("slider__element--active", "fadeIn");
+      reviewsActiveSlide = this.value - 1;
+      reviewsSlides[reviewsActiveSlide].classList.add("slider__element--active", "fadeIn");
     }
 
   })
