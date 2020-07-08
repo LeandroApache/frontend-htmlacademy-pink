abbec5848dc365683aabcd5033399bfc43e480a7
disorientationinspace <disorientationinspace@gmail.com>
2020-07-08 16:49:03 +0300

    Fixed BOLD fonts in Firefox Nightly

--- app/js/price-slider.js
+++ app/js/price-slider.js
@@ -1,11 +1,7 @@
 var priceSection = document.querySelector(".price");
-
 var priceRadioButtons = document.querySelectorAll(".slider__input-radio[type='radio'][name='price']");
-
 var priceVaries = ["price--left", "price--center", "price--right"];
 var activeVariety = "price--center";
-
-console.log(priceSection, priceRadioButtons, priceVaries, activeVariety);
 
 for (var i = 0; i < priceRadioButtons.length; i ++) {
   priceRadioButtons[i].addEventListener("click", function() {
